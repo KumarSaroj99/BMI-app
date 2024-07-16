19807e5a015b60f95cbe40de9878e6a1f786d5fc
KumarSaroj99 <sarojkumar.panda@aurionpro.com>
2024-07-16 15:17:03 +0530

    Add files via upload

---
+++ Person.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Xml.Linq;
+
+namespace BMI.Models
+{
+    internal class Person
+    {
+        //Default height and weight
+        static double HT = 5;
+        static int WEIGHT = 50;
+
+        //Properties
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public int Age { get; set; }
+        public double Ht { get; set; } = HT;
+        public int Weigth { get; set; } = WEIGHT;
+
+        //Constructors
+        public Person(int id, string name, int age)
+        {
+            Id = Id;
+            Name = name;
+            Age = age;
+        }
+        public Person(int id,string name,int age,double ht,int wegith):this(id,name,age)
+        {
+            Ht = ht;
+            Weigth = wegith;
+        }
+
+        //To calculate BMI
+        public double CalculateBMI()
+        {
+            Ht = Ht * 0.304;
+            return Weigth  / (Ht * Ht);
+        }
+
+        //To determine body type
+        public string BodyType(double bmi)
+        {
+            if (bmi < 18.5)
+            {
+                return "Underweight";
+            }
+            else if (bmi >= 18.5 && bmi < 24.9)
+            {
+                return "Normal";
+            }
+            else if (bmi >= 25 && bmi < 29.9)
+            {
+                return "Overweight";
+            }
+            else
+            {
+                return "Obese";
+            }
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using BMI.Models;
+
+namespace BMI
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Person person1 = new Person(100,"Raj",22,5.8,80);
+            Person person2 = new Person(101, "Samu", 23, 5.9, 75);
+
+            PrintPersonDetails(person1);
+        }
+        static void PrintPersonDetails(Person person)
+        {
+            string bodyType = person.BodyType(person.CalculateBMI());
+            Console.WriteLine($"Name : {person.Name}\n" +
+                $"Age : {person.Age}\n" +
+                $"Height in Metre : {person.Ht}\n" +
+                $"Weight in Kg : {person.Weigth}\n" +
+                $"Your Body Type : {bodyType}");
+        }
+    }
+}
